fd2ed394a9a4e75e5859f30e881a6238f14aecea
Rendi <rendyokts08@gmail.com>
2025-03-28 23:13:25 +0700

    Initialized

---
+++ src/main/java/pemrogramanvisual/absensi/Absensi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pemrogramanvisual.absensi;
+
+/**
+ *
+ * @author THINKPAD
+ */
+public class Absensi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
